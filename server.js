ac56295772e76b0921675ae7017d576d1e3bd1b8
Aidanc2000 <aidan@carvalho.org.uk>
2022-09-16 22:00:38 +0100

    fixed GET by content request

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 
 const { urlencoded, json } = require('body-parser');
 const app = express();
@@ -23,6 +24,7 @@ const noteSchema = new mongoose.Schema({
 });
 
 // ----------------------------------------------------------------
+app.use(morgan('tiny'));
 app.use(urlencoded({ extended: true }));
 app.use(json());
 //-----------------------------------------------------------------
