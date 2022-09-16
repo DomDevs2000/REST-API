5ab2ddf83196229bee34ca946e9a1f80d39e4bb5
Aidanc2000 <aidan@carvalho.org.uk>
2022-09-16 15:35:06 +0100

    added pagination to get request

--- app.js
+++ app.js
@@ -30,7 +30,10 @@ const Note = mongoose.model('Note', noteSchema);
 // ----------------
 app.get('/note', async (req, res) => {
 	try {
-		const notes = await Note.find({}).sort();
+		const notes = await Note.find({})
+			.sort()
+			.skip(req.query.page)
+			.limit(req.query.limit);
 
 		res.status(200).json(notes);
 	} catch (error) {
