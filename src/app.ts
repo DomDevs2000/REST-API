41257f7548f43b7004c4ea677c3caea4169bdf7a
DomDevs2000 <aidan@carvalho.org.uk>
2023-04-21 00:35:59 +0100

    added correct types to lambda handler

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,22 @@
 import dotenv from 'dotenv';
 import sls from 'serverless-http'
-dotenv.config();
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import {router} from './routes/notes.js';
+import mongoose from 'mongoose'
 
-import { router } from './routes/notes.js';
+import {
+    APIGatewayProxyEvent,
+    Context,
+} from "aws-lambda";
+dotenv.config();
 const app = express();
 
 // ----------------------------------------------------------------
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use('/notes', router);
 app.get('/', (req, res) => {
     res.send('Welcome to my Node.js CRUD REST API Project, please refer to the documentation at https://github.com/DomDevs2000/REST-API#readme');
@@ -20,11 +25,10 @@ app.set('json spaces', 2);
 //-----------------------------------------------------------------
 
 //----------------------------------------------------------------
-import mongoose from 'mongoose'
-import serverless from 'serverless-http'
+
 
 //@ts-ignore
-const MONGO_URI: string  = process.env.MONGO_URI
+const MONGO_URI: string = process.env.MONGO_URI
 
 
 const isValidMongoUrl = (mongoUrl: string | undefined): mongoUrl is string => {
@@ -42,15 +46,15 @@ const database = mongoose.connect(mongoUrl, {
 });
 
 
-export const db  = async (event: any, context: any) => {
+export const db = async (event: APIGatewayProxyEvent, context: Context) => {
     context.callbackWaitsForEmptyEventLoop = false;
-   database.then(async (connection) => {
-       console.log('Connected To MongoDB');
-       app.listen(process.env.PORT, () => console.log('Server Running...'));
-   })
-       .catch((error) => console.log(error));
+    database.then(async (connection) => {
+        console.log('Connected To MongoDB');
+        app.listen(process.env.PORT, () => console.log('Server Running...'));
+    })
+        .catch((error) => console.log(error));
 };
 
 export const server = sls(app)
-export { database };
+export {database};
 export {app}
